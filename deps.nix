4e170a4ae6b7d2c3371b18412067068a7e586936
haruki7049 <tontonkirikiri@gmail.com>
2024-09-17 22:28:50 +0900

    chore: nix flake update

--- deps.nix
+++ deps.nix
@@ -1,12 +1,20 @@
+{ fetchurl }:
+
 [
   {
     name = "spork";
-    url = "https://github.com/janet-lang/spork/archive/253a67e89dca695632283ef60f77851311c404c9.tar.gz";
-    hash = "sha256-oAIxARl8vOL7ULaDafLNywr/4yB7SBOUTEa983Mi2Qs=";
+    src = fetchurl {
+      url = "https://github.com/janet-lang/spork/archive/253a67e89dca695632283ef60f77851311c404c9.tar.gz";
+      hash = "sha256-oAIxARl8vOL7ULaDafLNywr/4yB7SBOUTEa983Mi2Qs=";
+    };
+    deps = [ ];
   }
   {
     name = "cmd";
-    url = "https://github.com/ianthehenry/cmd/archive/b4308de361d0f90dd96cc0f9a8dc6881e0e851c6.tar.gz";
-    hash = "sha256-Z6qj9M6Y1fYpxABYcm83SpRWL8nScrG+pxujB46X1Hs=";
+    src = fetchurl {
+      url = "https://github.com/ianthehenry/cmd/archive/b4308de361d0f90dd96cc0f9a8dc6881e0e851c6.tar.gz";
+      hash = "sha256-Z6qj9M6Y1fYpxABYcm83SpRWL8nScrG+pxujB46X1Hs=";
+    };
+    deps = [ ];
   }
 ]
